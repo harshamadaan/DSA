616f9e20d78760db9155b064221a5cf7536f07df
harsha arora <harshamadaan50@gmail.com>
2025-10-22 18:33:49 +0530

    Time: 50 ms (57.76%) | Memory: 55.2 MB (52.27%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,30 +1,63 @@
+// class Solution {
+//     public int totalFruit(int[] fruits) {
+//         int i = 0, j = 0;
+//         int k = 2;
+//         int n = fruits.length;
+//         int max = 0;
+//         if (n == 0) return 0;
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         while (j < n) {
+//             map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+
+//             if (map.size() <= k) {
+//                 max = Math.max(max, j - i + 1);
+//                 j++;
+//             } else {
+//                 while (map.size() > k) {
+//                     int leftFruit = fruits[i];
+//                     map.put(leftFruit, map.get(leftFruit) - 1);
+//                     if (map.get(leftFruit) == 0) {
+//                         map.remove(leftFruit);
+//                     }
+//                     i++;
+//                 }
+//                 j++;
+//             }
+//         }
+
+//         return max;
+//     }
+// }
+
+
+
+//or cuz we need to check only> case
+
 class Solution {
     public int totalFruit(int[] fruits) {
         int i = 0, j = 0;
-        int k = 2;
         int n = fruits.length;
         int max = 0;
-        if (n == 0) return 0;
-
         HashMap<Integer, Integer> map = new HashMap<>();
 
         while (j < n) {
             map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
 
-            if (map.size() <= k) {
-                max = Math.max(max, j - i + 1);
-                j++;
-            } else {
-                while (map.size() > k) {
-                    int leftFruit = fruits[i];
-                    map.put(leftFruit, map.get(leftFruit) - 1);
-                    if (map.get(leftFruit) == 0) {
-                        map.remove(leftFruit);
-                    }
-                    i++;
+            // If more than 2 fruit types, shrink the window
+            while (map.size() > 2) {
+                int leftFruit = fruits[i];
+                map.put(leftFruit, map.get(leftFruit) - 1);
+                if (map.get(leftFruit) == 0) {
+                    map.remove(leftFruit);
                 }
-                j++;
+                i++;
             }
+
+            // Window is valid: update max
+            max = Math.max(max, j - i + 1);
+            j++;
         }
 
         return max;
