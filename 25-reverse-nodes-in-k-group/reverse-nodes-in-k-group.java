57fe18d41808fc3a6b466e0b6087cc87f85e335d
harsha arora <harshamadaan50@gmail.com>
2026-03-07 15:57:56 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (74.34%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,54 +1,32 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
 
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null || k <= 1) return head;
-        return reverseK(head, k);
-    }
 
-    private ListNode reverseK(ListNode head, int k) {
+        ListNode temp = head;
+        int cnt = 0;
 
-        if (head == null) return null;
-
-        ListNode curr = head;
-        ListNode next = null;
-        ListNode prev = null;
-
-        int count = 0;
-
-        // Check if at least k nodes exist
-        ListNode check = head;
-        int nodes = 0;
-        while (nodes < k && check != null) {
-            check = check.next;
-            nodes++;
-        }
-        if (nodes < k) return head;
-
-        // Reverse first k nodes
-        while (curr != null && count < k) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-            count++;
+        // check if k nodes exist
+        while (cnt < k) {
+            if (temp == null) return head;
+            temp = temp.next;
+            cnt++;
         }
 
-        // Recurse for the rest
-        if (next != null) {
-            ListNode rest_head = reverseK(next, k);
-            head.next = rest_head;
+        // recursively process remaining list
+        ListNode prevNode = reverseKGroup(temp, k);
+
+        // reverse current group
+        temp = head;
+        cnt = 0;
+
+        while (cnt < k) {
+            ListNode next = temp.next;
+            temp.next = prevNode;
+            prevNode = temp;
+            temp = next;
+            cnt++;
         }
 
-        return prev;
+        return prevNode;
     }
 }
