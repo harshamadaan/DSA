3dcd85243948a69d14f47ca9d533752e629962bb
harsha arora <harshamadaan50@gmail.com>
2026-03-13 10:30:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (90.68%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -23,7 +23,7 @@ class Solution {
        
         ListNode preNode = null;
 
-        for (int i = 0; i <= right - left; i++) {
+        for (int i = 1; i <= right - left+1; i++) {
             ListNode nextNode = currNode.next;
             currNode.next = preNode;
             preNode = currNode;
