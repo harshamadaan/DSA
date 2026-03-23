46faf904dddd1d5c01fa9fb23172b7d8b09905d5
harsha arora <harshamadaan50@gmail.com>
2026-03-23 23:47:06 +0530

    Time: 2 ms (61.50%) | Memory: 43 MB (91.24%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,19 +1,31 @@
 class Solution {
+    //1st method
     public char findTheDifference(String s, String t) {
-        int[] freq=new int[26];
+        // int[] freq=new int[26];
+        // for(int i=0;i<s.length();i++){
+        //     freq[s.charAt(i)-'a']++;
+        // }
+        // for(int i=0;i<t.length();i++){
+        //     freq[t.charAt(i)-'a']--;
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(freq[i]<0){
+        //     return(char)(i+'a');
+        // }
+
+        // }
+        // return ' ';
+
+        //2nd
+        int XOR=0;
         for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
+            XOR^=s.charAt(i);
         }
         for(int i=0;i<t.length();i++){
-            freq[t.charAt(i)-'a']--;
+            
+            XOR^=t.charAt(i);
         }
-        for(int i=0;i<26;i++){
-            if(freq[i]<0){
-            return(char)(i+'a');
-        }
-
-        }
-        return ' ';
+        return (char)(XOR);
         
         
     }
