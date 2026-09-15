bf75be00bf718339253888086ede5e7534374e4b
harsha arora <harshamadaan50@gmail.com>
2026-09-16 01:00:10 +0530

    Time: 11 ms (5.10%) | Memory: 45.7 MB (10.36%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,34 +1,87 @@
+// class Solution {
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+//         List<List<Integer>> result = new ArrayList<>();
+
+//         Arrays.sort(nums);
+
+//         solve(nums, 0, new ArrayList<>(), result);
+
+//         return result;
+//     }
+
+//     void solve(int[] nums, int start, List<Integer> output,
+//                List<List<Integer>> result) {
+
+//         result.add(new ArrayList<>(output));
+
+//         for(int i = start; i < nums.length; i++) {
+
+//             // duplicate skip
+//             if(i > start && nums[i] == nums[i - 1]) {
+//                 continue;
+//             }
+
+//             // include
+//             output.add(nums[i]);
+
+//             solve(nums, i + 1, output, result);
+
+//             // backtrack
+//             output.remove(output.size() - 1);
+//         }
+//     }
+// }
+
+// prefer
+import java.util.*;
+
 class Solution {
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
 
+        List<Integer> input = new ArrayList<>();
+
+        for(int n : nums) {
+            input.add(n);
+        }
+
+        Collections.sort(input);
+
         List<List<Integer>> result = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
 
-        Arrays.sort(nums);
-
-        solve(nums, 0, new ArrayList<>(), result);
+        solve(input, output, result);
 
         return result;
     }
 
-    void solve(int[] nums, int start, List<Integer> output,
-               List<List<Integer>> result) {
+    static void solve(List<Integer> ip, List<Integer> op,
+                      List<List<Integer>> res) {
 
-        result.add(new ArrayList<>(output));
+        // Base case
+        if(ip.size() == 0) {
 
-        for(int i = start; i < nums.length; i++) {
-
-            // duplicate skip
-            if(i > start && nums[i] == nums[i - 1]) {
-                continue;
+            if(!res.contains(op)) {
+                res.add(new ArrayList<>(op));
             }
 
-            // include
-            output.add(nums[i]);
-
-            solve(nums, i + 1, output, result);
-
-            // backtrack
-            output.remove(output.size() - 1);
+            return;
         }
+
+        // EXCLUDE
+        List<Integer> op1 = new ArrayList<>(op);
+
+        // INCLUDE
+        List<Integer> op2 = new ArrayList<>(op);
+        op2.add(ip.get(0));
+
+        // Remove first element
+        List<Integer> newIp = new ArrayList<>(ip);
+        newIp.remove(0);
+
+        // Recursive calls
+        solve(newIp, op1, res);   // exclude
+        solve(newIp, op2, res);   // include
     }
 }
